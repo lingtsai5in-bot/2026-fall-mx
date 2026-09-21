a80dec07c3d8a065ded8063d38346bc30bb2ae84
Ling <lingtsai.5in@gmail.com>
2026-09-21 14:49:30 +0800

    921

--- 2026-fall-mx/students/ling/scripts/practice-2.js
+++ 2026-fall-mx/students/ling/scripts/practice-2.js
@@ -1,2 +1,17 @@
 const button = document.getElementById("my-button");
 console.log(button);
+
+const title = document.getElementById("my-title");
+console.log(title);
+
+function testMyButton(event) {
+    console.log("listen to my button!", event);
+}
+testMyButton("NOW");
+
+button.addEventListener("click", testMyButton);
+
+function testBody(event) {
+    console.log("listen to body!", event);
+}
+document.body.addEventListener("click", testBody);
